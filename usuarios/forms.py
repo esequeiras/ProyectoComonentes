4cb12150fac7125316022a50eb0c06716099f7ac
eli <esequeiras@ucenfotec.ac.cr>
2023-03-27 17:28:11 -0600

    completo el formulario para registro usuario

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -1,16 +1,22 @@
 from django import forms
+from .models import Usuario
 #from .models import Usuario
 #FloatField
 class UsuarioForm(forms.Form):
     nombre=forms.CharField(required=True)
     correo=forms.CharField(required=True)
     contrasena=forms.CharField(required=True)
-    fecha_nacimiiento=forms.DateTimeField()
+    fecha_nacimiciento=forms.DateField()
     contrasena_confirmacion=forms.CharField(required=True)
     identificacion=forms.CharField(required=True)
     establecimiento_de_salud=forms.CharField(required=True)
-    direccion=forms.CharField(required=True)
+    direccion=forms.CharField(required=True,widget=forms.Textarea())
+    class Meta:
+        widgets={
+            "fecha_nacimiciento": forms.SelectDateWidget()
+        }
 #relacion= forms.     label= algo, queryset=
+#widget
 
 
  
