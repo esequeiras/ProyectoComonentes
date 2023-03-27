6e9be615301074e62aa3849f6bda2b2aacefa1e6
eli <esequeiras@ucenfotec.ac.cr>
2023-03-27 11:51:07 -0600

    creacion de forms para el registro de usarios

---
+++ usuarios/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+#from .models import Usuario
+#FloatField
+class UsuarioForm(forms.Form):
+    nombre=forms.CharField(required=True)
+    correo=forms.CharField(required=True)
+    contrasena=forms.CharField(required=True)
+    fecha_nacimiiento=forms.DateTimeField()
+    contrasena=forms.CharField(required=True)
+    identificacion=forms.CharField(required=True)
+    establecimiento_de_salud=forms.CharField(required=True)
+    direccion=forms.CharField(required=True)
+#relacion= forms.     label= algo, queryset=
+
+
+ 
