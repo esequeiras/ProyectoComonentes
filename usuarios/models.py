c78305998a8b8eda270d8bcde427b748a7f40826
eli <esequeiras@ucenfotec.ac.cr>
2023-03-27 11:50:05 -0600

    agregue los campos necesarios en models usuario

--- usuarios/models.py
+++ usuarios/models.py
@@ -5,4 +5,8 @@ class Usuario(models.Model):
     nombre=models.TextField()
     correo=models.TextField()
     contrasena=models.TextField()
+    dierccion=models.TextField()
+    fecha_nacimiiento=models.DateField()
+    identificacion=models.TextField()
+    establecimiento_de_salud=models.TextField()
 
