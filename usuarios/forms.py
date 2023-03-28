8b700d84538efedec1d4fbfa6bb84d249c29cfb4
eli <esequeiras@ucenfotec.ac.cr>
2023-03-27 20:04:12 -0600

    agregue el DateInput(no implementado)

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -2,11 +2,17 @@ from django import forms
 from .models import Usuario
 #from .models import Usuario
 #FloatField
+
+class DateInput(forms.DateInput):
+    input_type='date'
+    
 class UsuarioForm(forms.Form):
     nombre=forms.CharField(required=True)
     correo=forms.CharField(required=True)
     contrasena=forms.CharField(required=True)
-    fecha_nacimiciento=forms.DateField()
+    #fecha_nacimiciento=forms.DateField(required=False,widget=DateInput)
+    #fecha_nacimiciento=forms.CharField()
+
     contrasena_confirmacion=forms.CharField(required=True)
     identificacion=forms.CharField(required=True)
     establecimiento_de_salud=forms.CharField(required=True)
