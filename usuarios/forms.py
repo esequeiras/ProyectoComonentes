22fd28e72a99143a6b5f9675a08b5277858a9ba6
eli <esequeiras@ucenfotec.ac.cr>
2023-03-28 11:38:21 -0600

    agregue el loginform

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -7,22 +7,21 @@ class DateInput(forms.DateInput):
     input_type='date'
     
 class UsuarioForm(forms.Form):
-    nombre=forms.CharField(required=True)
-    correo=forms.CharField(required=True)
-    contrasena=forms.CharField(required=True)
+    nombre=forms.CharField(label="Nombre",required=True)
+    correo=forms.CharField(label="Correo Electrónico",required=True)
+    contrasena=forms.CharField(label='Contraseña',required=True)
     #fecha_nacimiciento=forms.DateField(required=False,widget=DateInput)
     #fecha_nacimiciento=forms.CharField()
 
-    contrasena_confirmacion=forms.CharField(required=True)
-    identificacion=forms.CharField(required=True)
-    establecimiento_de_salud=forms.CharField(required=True)
-    direccion=forms.CharField(required=True,widget=forms.Textarea())
-    class Meta:
-        widgets={
-            "fecha_nacimiciento": forms.SelectDateWidget()
-        }
+    contrasena_confirmacion=forms.CharField(label='Confirmar contraseña',required=True)
+    identificacion=forms.CharField(label='Identificación',required=True)
+    establecimiento_de_salud=forms.CharField(label='Establecimiento de salud',required=True)
+    direccion=forms.CharField(required=True,widget=forms.Textarea(attrs={ 'rows':3, 'cols':30}))
+
 #relacion= forms.     label= algo, queryset=
 #widget
-
+class LoginForm(forms.Form):
+    correo=forms.CharField(label='Correo Electrónico',required=True)
+    contrasena=forms.CharField(label='Contraseña',required=True)
 
  
