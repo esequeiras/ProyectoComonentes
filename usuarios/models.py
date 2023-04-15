8056dd01c58a1e4312b1f37372215e1673c22e86
eli <esequeiras@ucenfotec.ac.cr>
2023-04-14 21:32:48 -0600

    se agrega la lista de medicamentos al perfil

--- usuarios/models.py
+++ usuarios/models.py
@@ -3,11 +3,18 @@ from django.db import models
 # Create your models here.
 class Usuario(models.Model):
     nombre=models.TextField()
-    correo=models.TextField(primary_key=True)
+    correo=models.TextField()
     contrasena=models.TextField()
     direccion=models.TextField()
     fecha_nacimiento=models.TextField(default='')
     identificacion=models.TextField()
     establecimiento_de_salud=models.TextField()
     tipo=models.TextField(default='paciente')
+
+class Medicina(models.Model):
+    medicamento=models.TextField()
+    fecha_prescripcion=models.TextField()
+    indicacion=models.TextField()
+    mese_tratamiento=models.TextField()
+    paciente=models.ForeignKey(Usuario,on_delete=models.CASCADE)
 
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from usuarios.models import Usuario 
+from usuarios.models import Usuario,Medicina
 from django.contrib import messages
 from django.template.loader import render_to_string
 from . import views
@@ -22,22 +22,25 @@ def usuario_inicio(request):
             my_lista=Usuario.objects.all()
             for us in my_lista:
                 
-                if us.tipo=="doctor":
-                    return doctor_view(request)
+                
                 
                 if us.correo==correo and us.contrasena==contrasena:
+                    if us.tipo=="doctor":
+                        return doctor_view(request)
+                    medicinas=Medicina.objects.filter(paciente_id=us.pk) #Filtro por llave foránea
+                    diagnosticos=Medicina.objects.filter(paciente_id=us.pk) #Filtro por llave foránea
+
                     datos={
+                        "id":us.pk,
                         "nombre":us.nombre,
                         "correo": us.correo,
-                        "identificacion":us.identificacion,
-                        "establecimiento_de_salud":us.establecimiento_de_salud,
-                        "direccion":us.direccion,
-                        "contrasena":us.contrasena,
-
-                    }
+                        "lista_medicinas": medicinas,
+                        "lista_diagnosticos":diagnosticos
+                    } 
+                    print(medicinas)
                     form=UsuarioForm(initial=datos)
                     #STRING_HTML=render_to_string("perfil-usuario.html",context=datos)#el nombre del html  
-                    return render(request,"perfil-usuario.html",{'form':form})
+                    return render(request,"perfil-usuario.html",datos)
 
             messages.add_message(request=request,level=messages.ERROR,message="Datos incorrectos, aun no tienes una cuenta?")     
         else:
@@ -59,7 +62,7 @@ def usuario_registro(request):
             usuario_nuevo.correo=form.cleaned_data['correo']
             usuario_nuevo.contrasena=form.cleaned_data['contrasena']
             usuario_nuevo.direccion=form.cleaned_data['direccion']
-            usuario_nuevo.fecha_nacimiento=form['fecha_nacimiento'].value
+            usuario_nuevo.fecha_nacimiento=form['fecha_nacimiento'].value()
             usuario_nuevo.identificacion=form.cleaned_data['identificacion']
             usuario_nuevo.establecimiento_de_salud=form.cleaned_data['establecimiento_de_salud']
             usuario_nuevo.save()#guardo en la bd local
@@ -70,8 +73,22 @@ def usuario_registro(request):
     return render(request,"registro-usuarios.html",{'form':form})
 
 
-def usuario_modificar(request,datos):
+def usuario_modificar(request,id):
+    usuario_log=Usuario.objects.get(id=id)
+    print(usuario_log.contrasena)
+
+    datos={
+        "nombre":usuario_log.nombre,
+        "correo": usuario_log.correo,
+        "contrasena":usuario_log.contrasena,
+        "fecha_nacimiento":usuario_log.fecha_nacimiento,
+        "establecimiento_de_salud":usuario_log.establecimiento_de_salud,
+        "identificacion":usuario_log.identificacion,
+        "direccion":usuario_log.direccion
+    }
     form=UsuarioForm(initial=datos)
+    #form["correo"].field.widget.attrs['disabled'] = True
+    #form["identificacion"].field.widget.attrs['disabled'] = True
 
     if request.method=="POST":
         # pasarle el request al formulario para que haga las validaciones del lado del servidor
@@ -80,23 +97,19 @@ def usuario_modificar(request,datos):
         if form.is_valid():
             print("informacion valida")
             usuario_nuevo=Usuario()
+            usuario_nuevo.pk=usuario_log.pk
             usuario_nuevo.nombre=form.cleaned_data['nombre']
-            usuario_nuevo.correo=form.cleaned_data['correo']
+            usuario_nuevo.correo=usuario_log.correo
             usuario_nuevo.contrasena=form.cleaned_data['contrasena']
             usuario_nuevo.direccion=form.cleaned_data['direccion']
-            usuario_nuevo.fecha_nacimiento=form['fecha_nacimiento'].value
-            usuario_nuevo.identificacion=form.cleaned_data['identificacion']
+            usuario_nuevo.fecha_nacimiento=form['fecha_nacimiento'].value()
+            usuario_nuevo.identificacion=usuario_log.identificacion
             usuario_nuevo.establecimiento_de_salud=form.cleaned_data['establecimiento_de_salud']
             usuario_nuevo.save()#guardo en la bd local
-            my_lista=Usuario.objects.all()
-            datos={
-                "lista_objetos":my_lista 
-            }
-            STRING_HTML=render_to_string("home-view.html",context=datos)#el nombre del html
-            return HttpResponse(STRING_HTML)           
+            return render(request,"modificar-usuario.html",{'form':form})
         else:
             print("No valida")
-    return render(request,"registro-usuarios.html",{'form':form})
+    return render(request,"modificar-usuario.html",{'form':form})
 
 
 def doctor_view(request):
