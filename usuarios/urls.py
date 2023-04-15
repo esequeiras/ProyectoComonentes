a927c342f6fe17907fe948f477168da93cd72bc7
eli <esequeiras@ucenfotec.ac.cr>
2023-04-14 21:33:47 -0600

    modificar usuario

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     #path('',home_view),#index
     path('',views.usuario_inicio,name='inicio'),
     path('registro/',views.usuario_registro, name="registro"),
-    path('modificar/',views.usuario_modificar, name="modificar"),
+    path('modificar/<int:id>',views.usuario_modificar, name="modificar"),
 
     path('lista/',views.doctor_view,name='lista'),
 
